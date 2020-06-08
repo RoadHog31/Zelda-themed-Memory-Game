0b6560c5049fa85dc7be5496e5f44385e3d2c219
SPHyrule <stphnpino1@aol.com>
2020-06-08 09:47:32 +0100

    a

---
+++ js/zeldagridgame.js
@@ -0,0 +1,54 @@
+/* Zelda Memory Game:
+
+1. Welcome screen on load. 
+2. Start Game button - symbols appear for 10 seconds then disapper.
+3. Player Name
+4. Total Score record.
+5. How many goes before game over? First to 6 when all cards have been matched. 
+6. 2 Player option.
+7. Rules page - THE RULES FOR PLAYING "MEMORY"
+
+    A. Mix up the cards.
+
+    B. Lay them in rows, face down.
+
+    C. Turn over any two cards.
+
+    D. If the two cards match, keep them.
+
+    E. If they don't match, turn them back over.
+
+    F. Remember what was on each card and where it was.
+
+    G. Watch and remember during the other player's turn.
+
+    H. The game is over when all the cards have been matched.
+
+    I. The player with the most matches wins. 
+
+8. Winner Banner! || Loser Banner!
+9. Does the first election match the second selection within the array? if yes leave symbols up if not then turn back!
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
